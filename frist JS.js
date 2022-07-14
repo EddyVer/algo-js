c8ea3473aaf25bd0936ab935ace88e24c1598cf0
Melonde-Zeus <vervoorteddy@outlook.com>
2022-07-14 14:27:48 +0200

    commit fin introduction

---
+++ frist JS.js
@@ -0,0 +1,9 @@
+const fisrtnumb = 52;
+const secondNumb= 34;
+const Name = "Eddy";
+const firstName = "Vervoort";
+let city = "Ransart";
+
+console.log("je m'appele " + firstName + Name + " et j'habite à " + city);
+console.log(fisrtnumb+secondNumb);
+console.log("hello");
